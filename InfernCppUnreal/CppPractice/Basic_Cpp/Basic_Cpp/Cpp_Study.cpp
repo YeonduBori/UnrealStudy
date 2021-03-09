ea99a06fd7f05287234c78ec6157dcf64f54439e
dongho5309 <dongho5309@naver.com>
2021-03-09 18:56:21 +0900

    In C# reigon -> Cpp #pragma region Infern day2(DataType, Initialize etc... easy for now)

---
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/Cpp_Study.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	#pragma region 산술연산
+	#pragma endregion
+}
--- InfernCppUnreal/HelloWorld.asm
+++ InfernCppUnreal/HelloWorld.asm
@@ -5,17 +5,29 @@ global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
     ;write your code here
-    ;PRINT_STRING msg
     
-    mov eax, 0x1234
-    mov rbx, 0x12345678
-    mov cl, 0xff
+    push 1
+    push 2
+    push 3
     
-    mov al, 0x00
-    mov rax, rdx
+    pop rax
+    pop rbx
+    pop rcx
     
     xor rax, rax
     ret
     
 ;section .data
-;    msg db 'Hello World', 0x00
+;
+
+;초기화 된 데이터
+;[변수이름] [크기] [초기값]
+;[크기] db(1byte) dw(2byte word) dd(4) dq(8)
+;section .data
+    ; - 리틀 엔디언 : 캐스팅에 유리함
+    ; - 빅 엔디언 : 숫자 비교에 유리 앞자리가 큰 숫자부터 나오기 때문?
+;초기화 되지 않은 데이터
+;[변수 이름] [크기] [개수]
+;[크기] resb(1) resb(2) resd(4) resq(8)
+section .bss
+    num resb 1
