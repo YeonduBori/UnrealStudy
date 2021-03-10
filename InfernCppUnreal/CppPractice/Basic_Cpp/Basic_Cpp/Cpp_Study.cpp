5a628cf57f1ef81c47b45d13a6670b587e2dccbf
dongho5309 <dongho5309@naver.com>
2021-03-10 21:33:32 +0900

    Cpp C# Difference enum 자체는 리얼 숫자랑 동치 비트 플래그는 보통 unsigned char 이용

--- InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/Cpp_Study.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/Cpp_Study.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+enum HandType
+{
+	SCISSORS = 0,
+	ROCK,
+	PAPER
+};
+
+#define DEFINE_SCISSORS 1//전처리기 1)전처리 2)컴파일 3)링크
+//#include -> 컴파일 전에 iostream 파일을 찾아서 해당 내용을 복붙
+
 int main()
 {
-	#pragma region 산술연산
+	#pragma region Region 나누기
+	HandType playerHand = SCISSORS;
+	//cpp에선 unsigned char 이용하여 비트 플래그 사용
 	#pragma endregion
 }
