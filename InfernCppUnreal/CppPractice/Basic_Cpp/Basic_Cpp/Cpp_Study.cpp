cd956892d5f2d175bd80941e9c9014538118bec7
dongho5309 <dongho5309@naver.com>
2021-03-12 22:00:23 +0900

    구현부, 선언부 분리하여 사용 선언부에선 함수의 시그니처를 정의하기 위함

--- InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/Cpp_Study.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/Cpp_Study.cpp
@@ -12,10 +12,35 @@ enum HandType
 #define DEFINE_SCISSORS 1//전처리기 1)전처리 2)컴파일 3)링크
 //#include -> 컴파일 전에 iostream 파일을 찾아서 해당 내용을 복붙
 
+//함수의 시그니처 선언
+void Func1();
+void Func2(int a, int b);
+void Func3(float);//이 형태도 가능
+
+void Func1()
+{
+	cout << "Func1" << endl;
+	Func2(1, 2);
+}
+
+void Func2(int a, int b)
+{
+	cout << "Func2" << endl;
+	Func3(10);
+}
+
+void Func3(float a)
+{
+	cout << "Func3" << endl;
+	cout << a + 0.1f << endl;
+}
+
 int main()
 {
 	#pragma region Region 나누기
 	HandType playerHand = SCISSORS;
 	//cpp에선 unsigned char 이용하여 비트 플래그 사용
 	#pragma endregion
+
+	Func1();
 }
