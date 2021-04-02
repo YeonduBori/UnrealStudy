6a0715a81267ed7337d5f6f59a92c7346cc706bd
dongho5309 <dongho5309@naver.com>
2021-04-02 15:00:15 +0900

    타입변환생성자(new) 명시적으로 쓰도록 선언도 가능

--- InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
@@ -20,6 +20,13 @@ public:
 		posX = knight.posX;
 		posY = knight.posY;
 	}
+
+	//인자를 1개만 받는 기타 생성자를 타입변환생성자
+	// 명시적 용도로만 사용할 것!
+	explicit Knight(int hp)
+	{
+		this->hp = hp;
+	}
 	//소멸자
 	~Knight()
 	{
@@ -29,6 +36,10 @@ public:
     //멤버 함수
     void Move(int y, int x);
     void Attack();
+	void HelloKnight(Knight knight)
+	{
+		cout << "Hello Knight" << endl;
+	}
     void Die()
     {
         hp = 0;
@@ -67,14 +78,11 @@ int main()
     k1.posY = 0;
     k1.posX = 0;
 
-    Knight k2;
-    k2.hp = 80;
-    k2.attack = 5;
-    k2.posY = 1;
-    k2.posX = 1;
-
-    k1.Move(2, 2);
-    k1.Attack();
-
-    k1.Die();
+	Knight k2(k1);
+	Knight k3 = k1;//복사생성자로 생성
+	
+	Knight k4;//기본생성자로 생성 후 복사
+	k4 = k1;//k3와 k4의 생성방식이 다름
+	Knight k5 = (Knight)1;// <- Knight(int hp)호출로 생성 명시적으로 변환하도록 선언
+	k5.HelloKnight((Knight)10);//Knight(int hp) 10 인 아이가 생성
 }
