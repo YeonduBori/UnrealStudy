3cb9cfd8b0e968fdebc83288972c3574ccaf6dc0
dongho5309 <dongho5309@naver.com>
2021-04-02 15:50:38 +0900

    Class Initializer Inheritance

--- InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
@@ -1,88 +1,158 @@
 ﻿#include <iostream>
 using namespace std;
 
-//데이터 + 가공 = 프로그램
+//class Knight
+//{
+//public:
+//	//생성자
+//	Knight()
+//	{
+//		cout << "Knight 기본 생성자 출력" << endl;
+//	}
+//	//복사생성자
+//	//일반적으로 '똑같은' 데이터를 가진 객체가 생성되길 기대함
+//	Knight(const Knight& knight)
+//	{
+//		hp = knight.hp;
+//		attack = knight.attack;
+//		posX = knight.posX;
+//		posY = knight.posY;
+//	}
+//
+//	//인자를 1개만 받는 기타 생성자를 타입변환생성자
+//	// 명시적 용도로만 사용할 것!
+//	explicit Knight(int hp)
+//	{
+//		this->hp = hp;
+//	}
+//	//소멸자
+//	~Knight()
+//	{
+//		cout << "Knight 소멸자 출력" << endl;
+//	}
+//
+//    //멤버 함수
+//    void Move(int y, int x);
+//    void Attack();
+//	void HelloKnight(Knight knight)
+//	{
+//		cout << "Hello Knight" << endl;
+//	}
+//    void Die()
+//    {
+//        hp = 0;
+//        cout << "Die" << endl;
+//    }
+//
+//public:
+//    //멤버 변수
+//    int hp;
+//    int attack;
+//    int posY;
+//    int posX;
+//    
+//};
+//
+//void Knight::Move(int y, int x)
+//{
+//    posY = y;
+//    posX = x;
+//    cout << "Move" << endl;
+//}
+//
+//void Knight::Attack()
+//{
+//
+//}
+//
+////Instantiate 객체를 만든다!
+//
+//
+//int main()
+//{
+//    Knight k1;
+//    k1.hp = 100;
+//    k1.attack = 10;
+//    k1.posY = 0;
+//    k1.posX = 0;
+//
+//	Knight k2(k1);
+//	Knight k3 = k1;//복사생성자로 생성
+//	
+//	Knight k4;//기본생성자로 생성 후 복사
+//	k4 = k1;//k3와 k4의 생성방식이 다름
+//	Knight k5 = (Knight)1;// <- Knight(int hp)호출로 생성 명시적으로 변환하도록 선언
+//	k5.HelloKnight((Knight)10);//Knight(int hp) 10 인 아이가 생성
+//}
 
-class Knight
+//객체지향 Object Oriented Programming
+//OOP
+//상속성
+//은닉성
+//다형성
+
+class Player
 {
 public:
-	//생성자
-	Knight()
+	Player()
 	{
-		cout << "Knight 기본 생성자 출력" << endl;
+		cout << "Player 생성자 호출" << endl;
 	}
-	//복사생성자
-	//일반적으로 '똑같은' 데이터를 가진 객체가 생성되길 기대함
-	Knight(const Knight& knight)
+	Player(int hp)
 	{
-		hp = knight.hp;
-		attack = knight.attack;
-		posX = knight.posX;
-		posY = knight.posY;
+		cout << "Player hp 생성자 호출" << endl;
 	}
-
-	//인자를 1개만 받는 기타 생성자를 타입변환생성자
-	// 명시적 용도로만 사용할 것!
-	explicit Knight(int hp)
+	~Player()
 	{
-		this->hp = hp;
+		cout << "Player 소멸자 호출" << endl;
 	}
-	//소멸자
-	~Knight()
-	{
-		cout << "Knight 소멸자 출력" << endl;
-	}
-
-    //멤버 함수
-    void Move(int y, int x);
-    void Attack();
-	void HelloKnight(Knight knight)
-	{
-		cout << "Hello Knight" << endl;
-	}
-    void Die()
-    {
-        hp = 0;
-        cout << "Die" << endl;
-    }
-
+	void Move() { cout << "Player Move 호출" << endl; }
+	void Attack() { cout << "Player Attack 호출" << endl; }
+	void Die() { cout << "Player Die 호출" << endl; }
 public:
-    //멤버 변수
-    int hp;
-    int attack;
-    int posY;
-    int posX;
-    
+	int _hp;
+	int _attack;
+	int _defence;
 };
 
-void Knight::Move(int y, int x)
+class Knight : public Player
 {
-    posY = y;
-    posX = x;
-    cout << "Move" << endl;
-}
+public:
+	Knight()
+	{
+		cout << "Knight 생성자 호출" << endl;
+	}
+	Knight(int stamina) : Player(100)
+	{
+		cout << "Knight Stamina 생성자 호출" << endl;
+	}
+	~Knight()
+	{
+		cout << "Knight 소멸자 호출" << endl;
+	}
+	void Move()
+	{
+		cout << "Knight Move 호출" << endl;
+	}
+public:
+	int _stamina;
+};
 
-void Knight::Attack()
+class Mage : public Player
 {
-
-}
-
-//Instantiate 객체를 만든다!
-
-
+public:
+	int _mp;
+};
 int main()
 {
-    Knight k1;
-    k1.hp = 100;
-    k1.attack = 10;
-    k1.posY = 0;
-    k1.posX = 0;
-
-	Knight k2(k1);
-	Knight k3 = k1;//복사생성자로 생성
-	
-	Knight k4;//기본생성자로 생성 후 복사
-	k4 = k1;//k3와 k4의 생성방식이 다름
-	Knight k5 = (Knight)1;// <- Knight(int hp)호출로 생성 명시적으로 변환하도록 선언
-	k5.HelloKnight((Knight)10);//Knight(int hp) 10 인 아이가 생성
+	Knight k;
+	k._hp = 100;
+	k._attack = 10;
+	k._defence = 5;
+	k._stamina = 100;
+	k.Move();
+	k.Player::Move();
+	//k.Attack();
+	//k.Die();
+	return 0;
 }
