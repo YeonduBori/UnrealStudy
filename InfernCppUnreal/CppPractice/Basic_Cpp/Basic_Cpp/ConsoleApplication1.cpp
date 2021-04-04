041088fe0ad38b510aafece91ee4ad21dbc57e63
dongho5309 <dongho5309@naver.com>
2021-04-04 23:58:46 +0900

    Cpp class Polymorphism

--- InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/Basic_Cpp/ConsoleApplication1.cpp
@@ -158,58 +158,120 @@ using namespace std;
 //}
 #pragma endregion
 
-class Car
+#pragma region 상속접근지정자, 은닉
+//class Car
+//{
+//public:
+//	void MoveHandle() {}
+//	void PushPedal() {}
+//	void OpenDoor() {}
+//	void TurnKey()
+//	{
+//		RunEngine();
+//	}
+//protected:
+//	void DisassembleCar() {}
+//	void RunEngine() {}
+//	void ConnectCircuit() {}
+//};
+//
+//
+////상속 접근 지정자 : 다음세대에게 부모 성질 어떻게 물려줄지
+//// public : 부모 유산 그대로 설계 상속(public -> public, protected -> protected)
+//// protected : 자손들에게만 상속(public -> protected, protected -> protected)
+//// private : 개인적인 상속(public -> private, protected -> private)
+//class SuperCar : public Car // 상속 접근 지정자
+//{
+//public:
+//	void PushRemoteController()
+//	{
+//		RunEngine();
+//	}
+//};
+//
+////캡슐화
+//class Berserker
+//{
+//public:
+//	int GetHp() { return _hp; }
+//	void SetHp(int hp)
+//	{
+//		_hp = hp;
+//		if (_hp <= 50)
+//			SetBerserkerMode();
+//	}
+//private:
+//	void SetBerserkerMode()
+//	{
+//		cout << "매우 강해짐" << endl;
+//	}
+//private:
+//	int _hp;
+//};
+# pragma endregion
+
+#pragma region 다형성
+//정적 바인딩(static Binding): 컴파일 시점에 결정
+//동적 바인딩(Dynamic Binding): 실행 시점에 결정
+//일반 함수는 정적바인딩을 사용
+//동적 바인딩을 원한다면? -> 가상 함수(virtual function)
+//실제 객체가 어떤 타입인지 어떻게 알고 가상함수를 호출하는가?
+// - 가상 함수 테이블(vftable)  주소값을 타고 가면 가상함수들의 주소를 담고 있다.
+// .vftable[] 4바이트(32bit OS) 8바이트(64bit OS)
+// [VMove][VDie]주소가 담겨있다. 가상함수들의 주소를 담고 있는 테이블
+// 가상함수테이블을 채워주는건 Knight의 생성자 선처리부분에서 Player의 vftable부분을 자기자신의 vftable로 채워준다.
+// 순수 가상 함수 : 구현은 없고 인터페이스만 전달
+// 추상 클래스 : 순수 가상 함수가 1개이상 포함된 클래스
+// 직접 객체를 만들 수 없게 됨
+class Player
 {
 public:
-	void MoveHandle() {}
-	void PushPedal() {}
-	void OpenDoor() {}
-	void TurnKey()
-	{
-		RunEngine();
-	}
-protected:
-	void DisassembleCar() {}
-	void RunEngine() {}
-	void ConnectCircuit(){}
-};
-
-
-//상속 접근 지정자 : 다음세대에게 부모 성질 어떻게 물려줄지
-// public : 부모 유산 그대로 설계 상속(public -> public, protected -> protected)
-// protected : 자손들에게만 상속(public -> protected, protected -> protected)
-// private : 개인적인 상속(public -> private, protected -> private)
-class SuperCar : public Car // 상속 접근 지정자
-{
+	void Move() { cout << "Move Player!" << endl; }
+	virtual void VMove() { cout << "Move Player" << endl; }
+	virtual void VDie() { cout << "VDie Player" << endl; }
+	virtual void VAttack() = 0;
 public:
-	void PushRemoteController()
-	{
-		RunEngine();
-	}
-};
-
-//캡슐화
-class Berserker
-{
-public:
-	int GetHp() { return _hp; }
-	void SetHp(int hp)
-	{
-		_hp = hp;
-		if (_hp <= 50)
-			SetBerserkerMode();
-	}
-private:
-	void SetBerserkerMode()
-	{
-		cout << "매우 강해짐" << endl;
-	}
-private:
 	int _hp;
+};
+
+class Knight : public Player
+{
+public:
+	void Move() { cout << "Knight Move!" << endl; }
+	//가상함수는 재정의하더라도 여전히 가상함수
+	virtual void VMove() { cout << "VMove Knight" << endl; }
+	virtual void VDie() { cout << "VDie Knight" << endl; }
+	virtual void VAttack() { cout << "VAttack Knight" << endl; }
+public:
+	int _stamina;
+};
+
+void MovePlayer(Player* player)
+{
+	player->VMove();
+	player->VDie();
+}
+
+void MoveKnight(Knight* knight)
+{
+	knight->Move();
+	knight->VDie();
+}
+
+class Mage : public Player
+{
+public:
+	int _mp;
 };
 
 int main()
 {
-	Car car;
+	//Player p;
+	//MovePlayer(&p);
+	
+	Knight k;
+	k.VAttack();
+	MovePlayer(&k);
 	return 0;
 }
+#pragma endregion
