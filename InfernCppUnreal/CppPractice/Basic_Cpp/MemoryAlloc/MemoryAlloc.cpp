75c975e7604b5e1fd180c5a92e516b9bbe888846
dongho5309 <dongho5309@naver.com>
2021-04-07 17:57:45 +0900

    Memory Alloc lecture Init

---
+++ InfernCppUnreal/CppPractice/Basic_Cpp/MemoryAlloc/MemoryAlloc.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+// 실행할 코드가 저장될 영역 -> 코드 영역
+// 전역/정적 변수 -> 데이터 영역
+// 지역 변수/매개 변수 -> 스택 영역
+// 동적 할당 -> 힙 영역
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
