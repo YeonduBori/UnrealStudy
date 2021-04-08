dcc39d5a0ffdfc9fa1f658964d96e3d41a85cb1a
dongho5309 <dongho5309@naver.com>
2021-04-08 13:06:20 +0900

    Memory Alloc lecture_1

--- InfernCppUnreal/CppPractice/Basic_Cpp/MemoryAlloc/MemoryAlloc.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/MemoryAlloc/MemoryAlloc.cpp
@@ -5,9 +5,19 @@ using namespace std;
 // 전역/정적 변수 -> 데이터 영역
 // 지역 변수/매개 변수 -> 스택 영역
 // 동적 할당 -> 힙 영역
+// 동적할당과 연관된 함수/ 연산자 : malloc, free, new, delete, new[], delete[]
+
+class Monster
+{
+public:
+    int _hp;
+    int _x;
+    int _y;
+};
+
 int main()
 {
-    cout << "Hello World!\n";
+    // 유저 영역 [메모장]
     return 0;
 }
 
