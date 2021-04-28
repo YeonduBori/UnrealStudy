e5c8e07895d9284d230610bd873dbe2fd93a3f06
dongho5309 <dongho5309@naver.com>
2021-04-28 18:03:14 +0900

    map, deq

---
+++ InfernCppUnreal/CppPractice/Basic_Cpp/STL_practice/STL_practice.cpp
@@ -0,0 +1,189 @@
+﻿#include <iostream>
+using namespace std;
+#include <deque>
+#include <vector>
+#include <map>
+
+#pragma region deque
+//int main()
+//{
+//    // 시퀀스 컨테이너(Sequence Container)
+//    // 데이터가 삽입 순서대로 나열되는 형태
+//    // vector list deque
+//
+//    // deque : double-ended queue 데크
+//    // [    ]
+//    // [    ]
+//
+//    //deque<int> dq;
+//    //dq.push_back(1);
+//    //dq.push_front(2);
+//    //cout << dq[0] << endl;
+//    //capacity X
+//    // vector와 마찬가지로 배열 기반으로 동작
+//    // 다만 메모리 할당 정책이 다르다
+//
+//    // vector
+//    // [        ]      ]
+//
+//    //deque
+//    // [    3 3]
+//    // [ 1  1  1  2]
+//    // [ 2     ]
+//
+//    vector<int> v(3, 1);
+//    deque<int> dq(3, 1);
+//
+//    v.push_back(2);
+//    v.push_back(2);
+//    dq.push_back(2);
+//    dq.push_back(2);
+//
+//    dq.push_front(3);
+//    dq.push_front(3);
+//
+//    // deque의 동작 원리
+//    // - 중간 삽입/삭제 (BAD / BAD)
+//    // - 처음/끝 삽입/삭제 (GOOD / GOOD)
+//    // - 임의 접근
+//    return 0;
+//}
+#pragma endregion
+
+#pragma region map
+class Player
+{
+public:
+	Player(): _playerId(0) { }
+	Player(int playerId) : _playerId(playerId) {}
+public:
+	int _playerId;
+};
+
+class Node
+{
+public:
+	Node* _left;
+	Node* _right;
+	//Data
+	int _key;
+	Player* _value;
+};
+
+int main()
+{
+	srand((unsigned int)time(nullptr));
+	////연관 컨테이너
+	////
+	//vector<Player*> v;
+	////10만명 입장
+	//for (int index = 0; index < 100000; index++)
+	//{
+	//	Player* p = new Player(index);
+	//	v.push_back(p);
+	//}
+
+	//// 5만명이 퇴장
+	//for (int index = 0; index < 50000; index++)
+	//{
+	//	int randIndex = rand() % v.size();
+	//	Player* p = v[randIndex];
+	//	delete p;
+
+	//	v.erase(v.begin() + randIndex);
+	//}
+
+	//// (ID = 2만 플레이어)가 (ID = 1만 Player)를 공격하고 싶어요
+	//// Q) ID = 1만인 Player를 찾아주세요.
+	//// A) 찾아온다.
+
+	//bool found = false;
+
+	//for (int index = 0; index < v.size(); index++)
+	//{
+	//	if (v[index]->_playerId == 10000)
+	//	{
+	//		found = true;
+	//		break;
+	//	}
+	//}
+
+	////vector, list의 치명적인 단점
+	//// -> 원하는 조건에 해당하는 데이터를 빠르게 찾을 수 없다.
+
+	//map : 균형 이진 트리(AVL)
+	// - 노드 기반
+
+	// (Key, value)
+	map<int, int> m;
+
+	//pair<map<int, int>::iterator, bool> ok;
+	//ok = m.insert(make_pair(1, 100));
+	//ok = m.insert(make_pair(1, 200));
+	//동일한 키가 있다면 덮어쓰는 것이 아니라 무시됨
+	//  
+	// 10만명
+	for (int index = 0; index < 100000; index++)
+	{
+		m.insert(pair<int, int>(index, index * 100));
+	}
+
+	// 5만명 퇴장
+	for (int index = 0; index < 50000; index++)
+	{
+		int randomValue = rand() % 50000;
+
+		//제거에 여러 버전이 있지만 일단은 Key값 이용
+		m.erase(randomValue);
+	}
+	//unsigned int count = 0;
+	//count = m.erase(10000); 삭제 성공시 1 실패시 0 (true, false?)
+	//count = m.erase(10000);
+	// Q) ID = 1만인 Player 찾고 싶다!
+	// A) 매우 빠르게 찾을 수 있음
+
+	map<int,int>::iterator findIt = m.find(10000);
+	if (findIt != m.end())
+	{
+		cout << "찾음" << endl;
+	}
+	else
+	{
+		cout << "못찾음" << endl;
+	}
+
+	// map 순회
+
+	for (map<int, int>::iterator it = m.begin(); it != m.end(); ++it)
+	{
+		pair<const int, int>& p = (*it);
+		int key = it->first;
+		int value = it->second;
+
+		cout << key << " " << value << endl;
+	}
+
+	//없으면 추가, 있으면 수정
+	map<int, int>::iterator findIt = m.find(10000);
+	if (findIt != m.end())
+	{
+		findIt->second = 200;
+	}
+	else
+	{
+		m.insert(make_pair(10000, 200));
+	}
+
+	//없으면 추가, 있으면 수정 v2
+	m[5] = 500;
+
+	// [] 연산자 사용할때 주의
+	// 대입을 하지 않더라도 (key/value) 형태의 데이터가 추가된다.
+	m.clear();
+	for (int index = 0; index < 10; index++)
+	{
+		cout << m[index] << endl;
+	}
+	return 0;
+}
+#pragma endregion
--- InfernCppUnreal/CppPractice/Basic_Cpp/list/list.cpp
+++ InfernCppUnreal/CppPractice/Basic_Cpp/list/list.cpp
@@ -67,196 +67,196 @@ using namespace std;
 #pragma endregion
 
 #pragma region list_Implement
-
-template<typename T>
-class Node
-{
-public:
-	Node(): _next(nullptr), _prev(nullptr), _data(T())
-	{
-	}
-
-	Node(const T& value) : _next(nullptr), _prev(nullptr), _data(value)
-	{
-
-	}
-public:
-	Node*	_prev;
-	Node*	_next;
-	T		_data;
-};
-
-template<typename T>
-class Iterator
-{
-public:
-	Iterator() :_node(nullptr)
-	{
-
-	}
-
-	Iterator(Node<T>* node):_node(node)
-	{
-
-	}
-
-	Iterator<T>& operator++()
-	{
-		_node = _node->_next;
-		return *this;
-	}
-
-	Iterator<T> operator++(int)
-	{
-		Iterator<T> temp = *this;
-		_node = _node->_next;
-		return temp;
-	}
-
-	Iterator<T>& operator--()
-	{
-		_node = _node->_prev;
-		return *this;
-	}
-
-	Iterator<T> operator--(int)
-	{
-		Iterator<T> temp = *this;
-		_node = _node->_prev;
-		return temp;
-	}
-
-	T& operator*()
-	{
-		return _node->_data;
-	}
-
-	bool operator==(const Iterator& right)
-	{
-		return _node == right._node;
-	}
-
-	bool operator!=(const Iterator& right)
-	{
-		return _node != right._node;
-	}
-public:
-	Node<T>* _node;
-};
-
-//[header]
-template<typename T>
-class List
-{
-public:
-	List() : _size(0)
-	{
-		_header = new Node<T>();
-		_header->_next = _header;
-		_header->_prev = _header;
-	}
-
-	~List()
-	{
-		//TODO
-		while (_size > 0)
-			pop_back();
-
-		delete _header;
-	}
-
-	void push_back(const T& value)
-	{
-		AddNode(_header, value);
-	}
-
-	Node<T>* AddNode(Node<T>* before, const T& value)
-	{
-		Node<T>* node = new Node<T>(value);
-		node->_prev = before->_prev->_next;
-		node->_next = before->_prev;
-		before->_prev = node;
-
-		_size++;
-
-		return node;
-	}
-	
-	//[before] <-> [node] <-> [next]
-	//[before]		<->		  [next]
-	Node<T>* RemoveNode(Node<T>* node)
-	{
-		Node<T>* prevNode = node->_prev;
-		Node<T>* nextNode = node->_next;
-		prevNode->_next = node->_next;
-		nextNode->_prev = node->_prev;
-
-		delete node;
-		_size--;
-
-		return nextNode;
-	}
-
-	void pop_back()
-	{
-		RemoveNode(_header->_prev);
-	}
-
-	int size()
-	{
-		return _size;
-	}
-
-public:
-	typedef Iterator<T> iterator;
-
-	iterator begin(){ return iterator(_header->_next);}
-	iterator end(){ return iterator(_header);}
-
-	iterator insert(iterator it, const T& value)
-	{
-		Node<T>* node = AddNode(it._node, value);
-		return iterator(node);
-	}
-
-	iterator erase(iterator it)
-	{
-		Node<T>* node = RemoveNode(it._node);
-		return iterator(node);
-	}
-public:
-	Node<T>* _header;
-	int _size;
-};
-
-int main()
-{
-	List<int> li;
-
-	List<int>::iterator eraseIt;
-
-	for (int index = 0; index < 10; index++)
-	{
-		if (index == 5)
-		{
-			eraseIt = li.insert(li.end(), index);
-		}
-		else
-		{
-			li.push_back(index);
-		}
-	}
-
-	li.pop_back();
-
-	li.erase(eraseIt);
-
-	for (List<int>::iterator it = li.begin(); it != li.end(); ++it)
-	{
-		cout << (*it) << endl;
-	}
-
-	return 0;
-}
+//
+//template<typename T>
+//class Node
+//{
+//public:
+//	Node(): _next(nullptr), _prev(nullptr), _data(T())
+//	{
+//	}
+//
+//	Node(const T& value) : _next(nullptr), _prev(nullptr), _data(value)
+//	{
+//
+//	}
+//public:
+//	Node*	_prev;
+//	Node*	_next;
+//	T		_data;
+//};
+//
+//template<typename T>
+//class Iterator
+//{
+//public:
+//	Iterator() :_node(nullptr)
+//	{
+//
+//	}
+//
+//	Iterator(Node<T>* node):_node(node)
+//	{
+//
+//	}
+//
+//	Iterator<T>& operator++()
+//	{
+//		_node = _node->_next;
+//		return *this;
+//	}
+//
+//	Iterator<T> operator++(int)
+//	{
+//		Iterator<T> temp = *this;
+//		_node = _node->_next;
+//		return temp;
+//	}
+//
+//	Iterator<T>& operator--()
+//	{
+//		_node = _node->_prev;
+//		return *this;
+//	}
+//
+//	Iterator<T> operator--(int)
+//	{
+//		Iterator<T> temp = *this;
+//		_node = _node->_prev;
+//		return temp;
+//	}
+//
+//	T& operator*()
+//	{
+//		return _node->_data;
+//	}
+//
+//	bool operator==(const Iterator& right)
+//	{
+//		return _node == right._node;
+//	}
+//
+//	bool operator!=(const Iterator& right)
+//	{
+//		return _node != right._node;
+//	}
+//public:
+//	Node<T>* _node;
+//};
+//
+////[header]
+//template<typename T>
+//class List
+//{
+//public:
+//	List() : _size(0)
+//	{
+//		_header = new Node<T>();
+//		_header->_next = _header;
+//		_header->_prev = _header;
+//	}
+//
+//	~List()
+//	{
+//		//TODO
+//		while (_size > 0)
+//			pop_back();
+//
+//		delete _header;
+//	}
+//
+//	void push_back(const T& value)
+//	{
+//		AddNode(_header, value);
+//	}
+//
+//	Node<T>* AddNode(Node<T>* before, const T& value)
+//	{
+//		Node<T>* node = new Node<T>(value);
+//		node->_prev = before->_prev->_next;
+//		node->_next = before->_prev;
+//		before->_prev = node;
+//
+//		_size++;
+//
+//		return node;
+//	}
+//	
+//	//[before] <-> [node] <-> [next]
+//	//[before]		<->		  [next]
+//	Node<T>* RemoveNode(Node<T>* node)
+//	{
+//		Node<T>* prevNode = node->_prev;
+//		Node<T>* nextNode = node->_next;
+//		prevNode->_next = node->_next;
+//		nextNode->_prev = node->_prev;
+//
+//		delete node;
+//		_size--;
+//
+//		return nextNode;
+//	}
+//
+//	void pop_back()
+//	{
+//		RemoveNode(_header->_prev);
+//	}
+//
+//	int size()
+//	{
+//		return _size;
+//	}
+//
+//public:
+//	typedef Iterator<T> iterator;
+//
+//	iterator begin(){ return iterator(_header->_next);}
+//	iterator end(){ return iterator(_header);}
+//
+//	iterator insert(iterator it, const T& value)
+//	{
+//		Node<T>* node = AddNode(it._node, value);
+//		return iterator(node);
+//	}
+//
+//	iterator erase(iterator it)
+//	{
+//		Node<T>* node = RemoveNode(it._node);
+//		return iterator(node);
+//	}
+//public:
+//	Node<T>* _header;
+//	int _size;
+//};
+//
+//int main()
+//{
+//	List<int> li;
+//
+//	List<int>::iterator eraseIt;
+//
+//	for (int index = 0; index < 10; index++)
+//	{
+//		if (index == 5)
+//		{
+//			eraseIt = li.insert(li.end(), index);
+//		}
+//		else
+//		{
+//			li.push_back(index);
+//		}
+//	}
+//
+//	li.pop_back();
+//
+//	li.erase(eraseIt);
+//
+//	for (List<int>::iterator it = li.begin(); it != li.end(); ++it)
+//	{
+//		cout << (*it) << endl;
+//	}
+//
+//	return 0;
+//}
 #pragma endregion
 
