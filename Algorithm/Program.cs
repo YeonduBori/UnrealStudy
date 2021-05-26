e2a544df5c7245969a9e9e7192a52c14db356b9d
YeonduBori <dongho5309@naver.com>
2021-05-26 21:52:13 +0900

    Kruskal Programmers Example Solved

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Algorithm
 {
@@ -380,26 +381,106 @@ namespace Algorithm
     #endregion
 
     #region Dijkstra Algorithm
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[,] nodeConnection = new int[6, 6];
+    //        nodeConnection[0, 1] = 8;
+    //        nodeConnection[0, 2] = 1;
+    //        nodeConnection[0, 3] = 2;
+    //        nodeConnection[2, 1] = 5;
+    //        nodeConnection[2, 3] = 2;
+    //        nodeConnection[3, 4] = 3;
+    //        nodeConnection[3, 5] = 5;
+    //        nodeConnection[4, 5] = 1;
+    //        nodeConnection[5, 0] = 5;
+
+    //        int[] result = DijkstraResult(nodeConnection, 0);
+    //        foreach(var number in result)
+    //        {
+    //            Console.WriteLine(number);
+    //        }
+    //    }
+
+    //    public static int[] DijkstraResult(int[,] connection, int startNode)
+    //    {
+    //        int[] resultArray = new int[connection.GetLength(0)];
+    //        bool[] checkComplete = new bool[connection.GetLength(0)];
+    //        for (int index = 0; index < resultArray.Length; index++)
+    //        {
+    //            if (startNode == index)
+    //                continue;
+    //            else
+    //                resultArray[index] = int.MaxValue;
+    //        }
+
+    //        List<Node> PrimaryQueue = new List<Node>();
+    //        Node node = new Node(startNode, 0);
+    //        PrimaryQueue.Add(node);
+
+    //        while (PrimaryQueue.Count != 0)
+    //        {
+    //            Node node1 = PrimaryQueue[0];
+    //            for (int index = 0; index < resultArray.Length; index++)
+    //            {
+    //                if (connection[node1.index, index] != 0)
+    //                {
+    //                    if (node1.length + connection[node1.index, index] < resultArray[index])
+    //                    {
+    //                        resultArray[index] = node1.length + connection[node1.index, index];
+    //                    }
+    //                }
+    //            }
+    //            checkComplete[node1.index] = true;
+    //            PrimaryQueue.Remove(node1);
+
+    //            for (int index = 0; index < resultArray.Length; index++)
+    //            {
+    //                if (resultArray[index] != 0 && resultArray[index] != int.MaxValue && !checkComplete[index])
+    //                {
+    //                    PrimaryQueue.Add(new Node(index, resultArray[index]));
+    //                }
+    //            }
+    //            PrimaryQueue = PrimaryQueue.OrderBy(n => n.length).ToList();
+    //        }
+
+    //        return resultArray;
+    //    }
+
+    //    public class Node
+    //    {
+    //        public int index;
+    //        public int length;
+    //        public Node(int i, int l)
+    //        {
+    //            index = i;
+    //            length = l;
+    //        }
+    //    }
+    //    #endregion
+    //}
+    #endregion
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] nodeConnection = new int[6, 6];
-            nodeConnection[0, 1] = 8;
-            nodeConnection[0, 2] = 1;
-            nodeConnection[0, 3] = 2;
-            nodeConnection[2, 1] = 5;
-            nodeConnection[2, 3] = 2;
-            nodeConnection[3, 4] = 3;
-            nodeConnection[3, 5] = 5;
-            nodeConnection[4, 5] = 1;
-            nodeConnection[5, 0] = 5;
+            //int[,] nodeConnection = new int[6, 6];
+            //nodeConnection[0, 1] = 8;
+            //nodeConnection[0, 2] = 1;
+            //nodeConnection[0, 3] = 2;
+            //nodeConnection[2, 1] = 5;
+            //nodeConnection[2, 3] = 2;
+            //nodeConnection[3, 4] = 3;
+            //nodeConnection[3, 5] = 5;
+            //nodeConnection[4, 5] = 1;
+            //nodeConnection[5, 0] = 5;
 
-            int[] result = DijkstraResult(nodeConnection, 0);
-            foreach (var number in result)
-            {
-                Console.WriteLine(number);
-            }
+            //int[] result = DijkstraResult(nodeConnection, 0);
+            //foreach (var number in result)
+            //{
+            //    Console.WriteLine(number);
+            //}
             //List<List<int>> arr = new List<List<int>>();
             //arr.Add(new List<int> { 11, 2, 4 });
             //arr.Add(new List<int> { 4, 5, 6 });
@@ -415,63 +496,88 @@ namespace Algorithm
             //    rightToLeft += arr[index][arr.Count -1 - index];
             //}
             //Console.WriteLine(Math.Abs(leftToRight - rightToLeft));
+            Solution solution = new Solution();
+            int[,] costs = { { 0, 1, 1 },{ 0, 2, 2 },{ 1, 2, 5 },{ 1, 3, 1 },{ 2, 3, 8 } };
+            Console.WriteLine(solution.solution(4,costs));
+
         }
-
-        public static int[] DijkstraResult(int[,] connection, int startNode)
-        {
-            int[] resultArray = new int[connection.GetLength(0)];
-            bool[] checkComplete = new bool[connection.GetLength(0)];
-            for (int index = 0; index < resultArray.Length; index++)
-            {
-                if (startNode == index)
-                    continue;
-                else
-                    resultArray[index] = int.MaxValue;
-            }
-
-            List<Node> PrimaryQueue = new List<Node>();
-            Node node = new Node(startNode, 0);
-            PrimaryQueue.Add(node);
-
-            while (PrimaryQueue.Count != 0)
-            {
-                Node node1 = PrimaryQueue[0];
-                for (int index = 0; index < resultArray.Length; index++)
-                {
-                    if (connection[node1.index, index] != 0)
-                    {
-                        if (node1.length + connection[node1.index, index] < resultArray[index])
-                        {
-                            resultArray[index] = node1.length + connection[node1.index, index];
-                        }
-                    }
-                }
-                checkComplete[node1.index] = true;
-                PrimaryQueue.Remove(node1);
-
-                for (int index = 0; index < resultArray.Length; index++)
-                {
-                    if (resultArray[index] != 0 && resultArray[index] != int.MaxValue && !checkComplete[index])
-                    {
-                        PrimaryQueue.Add(new Node(index, resultArray[index]));
-                    }
-                }
-                PrimaryQueue = PrimaryQueue.OrderBy(n => n.length).ToList();
-            }
-
-            return resultArray;
-        }
-
-        public class Node
-        {
-            public int index;
-            public int length;
-            public Node(int i, int l)
-            {
-                index = i;
-                length = l;
-            }
-        }
-        #endregion
     }
+
+
+    public class Solution
+    {
+        public int solution(int n, int[,] costs)
+        {
+
+            List<Edge> list = new List<Edge>();
+            for(int index = 0; index < costs.GetLength(0); index++)
+            {
+                list.Add(new Edge(costs[index, 0], costs[index, 1], costs[index, 2]));
+            }
+
+            // 간선의 비용을 기준으로 오름차순 정렬
+            list = list.OrderBy(o => o.distance).ToList();
+
+            // 각 정점이 포함된 그래프가 어디인지 저장
+            int[] parent = new int[n];
+            for (int index = 0; index < n; index++)
+            {
+                parent[index] = index;
+            }
+
+            // 거리의 합을 0으로 초기화
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                // 동일한 부모를 가르키지 않는 경우, 즉 사이클이 발생하지 않을 때만 선택
+                if (!FindParent(parent, list[i].node[0], list[i].node[1]))
+                {
+                    sum += list[i].distance;
+                    UnionParent(parent, list[i].node[0], list[i].node[1]);
+                }
+            }
+            return sum;
+        }
+
+        public int GetParent(int[] parent, int x)
+        {
+            if (parent[x] == x) return x;
+            return parent[x] = GetParent(parent, parent[x]);
+        }
+
+        // 각 부모 노드를 합침
+        public void UnionParent(int[] parent, int a, int b)
+        {
+            a = GetParent(parent, a);
+            b = GetParent(parent, b);
+            if (a < b) parent[b] = a;
+            else parent[a] = b;
+        }
+
+        // 같은 부모 노드를 가지는지 확인
+        public bool FindParent(int[] parent, int a, int b)
+        {
+            a = GetParent(parent, a);
+            b = GetParent(parent, b);
+            if (a == b) return true;
+            else return false;
+        }
+    }
+
+
+    // 간선 클래스
+    class Edge
+    {
+        public int[] node = new int[2];
+        public int distance;
+
+        public Edge(int a, int b, int distance)
+        {
+            node[0] = a;
+            node[1] = b;
+            this.distance = distance;
+        }
+    }
+
+
 }
